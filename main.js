077b50ad75805bf1990944425d917b0a123d8791
David Metcalf <61023941+gloriousLoaf@users.noreply.github.com>
2024-08-23 17:56:03 +0000

    scaffold form submission

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-document.querySelector('#submit-employee').addEventListener('click', createEmployee);
-document.querySelector('#submit-asset').addEventListener('click', createAsset);
-
-const empFormData = JSON.stringify(Object.fromEntries(new FormData(document.querySelector('form#emp-form'))));
-const assetFormData = JSON.stringify(Object.fromEntries(new FormData(document.querySelector('form#emp-form'))));
-
-function createEmployee(e) {
+// handle form data
+document.querySelector('#emp-form').addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('hello employee');
+    const empFormData = JSON.stringify(Object.fromEntries(new FormData(document.querySelector('form#emp-form'))));
+    console.log('emp data', empFormData);
+});
+
+document.querySelector('#asset-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const assetFormData = JSON.stringify(Object.fromEntries(new FormData(document.querySelector('form#asset-form'))));
+    console.log('asset data:', assetFormData)
+});
+
+// employee crud
+function createEmployee(data) {
+    // post
 }
 
-function createAsset(e) {
-    e.preventDefault();
-    console.log('hello asset');
+function updateEmployee(data) {
+    // put
+}
+
+function deleteEmployee(data) {
+    // delete
+}
+
+// asset crud
+function createAsset(data) {
+    // post
+}
+function updateAsset(data) {
+    // post
+}
+function deleteAsset(data) {
+    // delet
 }
