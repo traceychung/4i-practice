407d859fce832accd77c88c4c03248e591a4fbad
tracey chung <tchung97@gmail.com>
2025-03-30 20:09:05 -0700

    Assets try/excepts + status code

--- backend/assets/views.py
+++ backend/assets/views.py
@@ -13,26 +13,37 @@ def list_assets(request):
     if request.method == "GET":
         assets = Asset.objects.all()
         data = list(Asset.objects.values())
-        return JsonResponse(data, safe=False)
+        return JsonResponse(data, safe=False, status=200)
     else:
         data = json.loads(request.body)
         new_asset = Asset.objects.create(**data)
         data = list(Asset.objects.values())
-        return JsonResponse(data, safe=False)
+        return JsonResponse(data, safe=False, status=200)
 
 @csrf_exempt
 @require_http_methods(["GET","PUT","DELETE"])
 def get_asset(request, pk):
     if request.method == "GET":
-        asset = Asset.objects.get(id=pk)
-        data = asset_to_dict(asset)
-        return JsonResponse(data, safe=False)
+        try:
+            asset = Asset.objects.get(id=pk)
+            data = asset_to_dict(asset)
+            return JsonResponse(data, safe=False, status=200)
+        except:
+            return JsonResponse({"message": "ID does not exist"}, safe=False, status=404)
+
     elif request.method == "PUT":
-        data = json.loads(request.body)
-        update_asset = Asset.objects.filter(id=pk).update(**data)
-        asset = Asset.objects.get(id=pk)
-        data = asset_to_dict(asset)
-        return JsonResponse(data, safe=False)
+        try:
+            data = json.loads(request.body)
+            update_asset = Asset.objects.filter(id=pk).update(**data)
+            asset = Asset.objects.get(id=pk)
+            data = asset_to_dict(asset)
+            return JsonResponse(data, safe=False, status=200)
+        except:
+            return JsonResponse({"message": "ID does not exist"}, safe=False, status=404)
     else:
-        Asset.objects.filter(id=pk).delete()
-        return JsonResponse({"message": "Asset was successfully deleted"}, safe=False)
+        try:
+            asset = Asset.objects.get(id=pk)
+            Asset.objects.filter(id=pk).delete()
+            return JsonResponse({"message": "Asset was successfully deleted"}, safe=False)
+        except:
+            return JsonResponse({"message": "ID does not exist"}, safe=False, status=404)
